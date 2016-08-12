2ecb382ad8524314d05539d948fa1f3c0346685a
Basarat Ali Syed <Basarat.AliSyed@auspost.com.au>
2016-08-12 15:33:19 +1000

    wip

---
+++ ts/2 unions/1 concept.ts
@@ -0,0 +1,11 @@
+function sendEmail(address: string) {
+  console.log('Sending:',address); // Pretend
+}
+
+function beAwesome(person: { name: string, email: string }) {
+  
+}
+
+
+/** Module */
+export { sendEmail };
